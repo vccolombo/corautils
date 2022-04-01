84637ff9b5f7fb97b6897a848479984d9c678333
Víctor Colombo <victorcora98@gmail.com>
2022-04-01 20:52:43 -0300

    Add 120 columns width
    cargo fmt is a behaving a bit weird, even when setting the value to 100
    it breaks the line before 80. clap might be doing something behind the
    scenes and confusing the fmt tool?

--- src/bin/echo.rs
+++ src/bin/echo.rs
@@ -6,20 +6,12 @@ struct Cli {
     #[clap(value_name = "STRING", multiple_values = true)]
     string: Vec<String>,
 
-    #[clap(
-        short = 'n',
-        help = "Do not output the trailing newline",
-        takes_value = false
-    )]
+    #[clap(short = 'n', help = "Do not output the trailing newline", takes_value = false)]
     omit_newline: bool,
 }
 
 fn main() {
     let cli = Cli::parse();
 
-    print!(
-        "{}{}",
-        cli.string.join(" "),
-        if cli.omit_newline { "" } else { "\n" }
-    );
+    print!("{}{}", cli.string.join(" "), if cli.omit_newline { "" } else { "\n" });
 }
